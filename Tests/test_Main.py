8485c04dedb6e723e43893646f07070a129d2cfa
Priya Sawant <priyasawant1984@gmail.com>
2021-06-22 17:51:58 -0500

    updated with extract_data function

--- Tests/test_Main.py
+++ Tests/test_Main.py
@@ -13,6 +13,7 @@ Dict_state = {}
 Dict_district = {}
 Dict_resourse = {}
 Dict_data = {}
+Dict_excel_state = {}
 
 def test_setup():
     Intantiate.driver.get(Testlinks.BASE_URL)
@@ -30,23 +31,38 @@ def test_extract_state():
        # sleep(2)
        # test_extract_district()
 
-
 def test_loadExcel():
-    book = openpyxl.load_workbook("..//Test Data//MIssionHumanData.xlsx")
+    book = openpyxl.load_workbook("C:\\Training matrial\\Project Covid\\States.xlsx")
     sheet = book.active
     cell = sheet.cell(row=2, column=1)
     temp = []
     res = dict()
-    for i in range(1, sheet.max_row + 1):  # to get rows
-        # if sheet.cell(row=i, column=2).value == "Andhra Pradesh":
-        for j in range(1, sheet.max_column + 1):  # to get column
-            # print(sheet.cell(row=i, column=1).value)
-            Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
-            # Remove duplicate values in dictionary
-            # Using dictionary comprehension
-        temp = {val: key for key, val in Dict.items()}
-        res = {val: key for key, val in temp.items()}
-        print(res)
+    for i in range(2, sheet.max_row + 1):
+            #print(sheet.cell(row=i, column=1).value)
+            Dict["State"] = sheet.cell(row=i, column=1).value
+            print(Dict)
+
+
+# def test_loadExcel():
+#     book = openpyxl.load_workbook("..//Test Data//MIssionHumanData.xlsx")
+#     sheet = book.active
+#     cell = sheet.cell(row=2, column=1)
+#     temp = []
+#     res = dict()
+#     for i in range(1, sheet.max_row + 1):  # to get rows
+#         # if sheet.cell(row=i, column=2).value == "Andhra Pradesh":
+#         Dict_excel_state[sheet.cell(row=i, column=2).value]
+#         print(Dict_excel_state)
+#         # for j in range(1, sheet.max_column + 1):  # to get column
+#         #     # print(sheet.cell(row=i, column=1).value)
+#         #
+#         #     Dict[sheet.cell(row=1, column=j).value] = sheet.cell(row=i, column=j).value
+#         #
+#         #     # Remove duplicate values in dictionary
+#         #     # Using dictionary comprehension
+#         # temp = {val: key for key, val in Dict.items()}
+#         # res = {val: key for key, val in temp.items()}
+#         # print(res)
 
 
 def test_StateCompare():
@@ -70,7 +86,7 @@ def test_extract_district():
               district.find_element_by_xpath("//div[@class='location-column']")
               Dict_district["District"] = district.text
               print(Dict_district)
-              district.click()
+              Intantiate.driver.find_element_by_xpath("//*[@id='root']/div/div/div[3]/div").click()
               test_extract_resourse()
               sleep(2)
 
@@ -86,14 +102,19 @@ def test_extract_resourse():
         test_extract_data()
 
 
-def test_extract_data():
+def test_extract_data()
     data_count = Intantiate.driver.find_elements_by_xpath("//div[@class='MuiCardContent-root lead-container']")
     print(len(data_count))
     for data in data_count:
         data.find_element_by_xpath("//div[@class='MuiCardContent-root lead-container']")
         Dict_data["data"] = data.text
         print(Dict_data)
-    Intantiate.driver.find_element_by_xpath("//div[@class='bookmarked' and contains(text(), 'MissionHumane.org')]").click()
+
+    Intantiate.driver.find_element_by_xpath("//img[@alt='back']").click()
+    try:
+        Intantiate.driver.find_element_by_xpath("//div[@class='sc-hKFxyN kksiKu']").is_displayed()
+    except:
+        print("stale error")
     sleep(2)
 
 # closing browser
