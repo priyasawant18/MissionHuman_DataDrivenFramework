7bf830954727724025a31cbe3337fa1efa8d8c20
Priya Sawant <priyasawant1984@gmail.com>
2021-06-21 23:48:52 -0500

    updated with data

--- Tests/test_Main.py
+++ Tests/test_Main.py
@@ -1,4 +1,9 @@
 from time import sleep
+
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+
 from Utils.Utilities import Testlinks
 from Utils.Utilities import Intantiate
 import openpyxl
@@ -6,6 +11,8 @@ import openpyxl
 Dict = {}
 Dict_state = {}
 Dict_district = {}
+Dict_resourse = {}
+Dict_data = {}
 
 def test_setup():
     Intantiate.driver.get(Testlinks.BASE_URL)
@@ -20,14 +27,7 @@ def test_extract_state():
        #print(Dict1)
        val.click()
        sleep(2)
-       district_name = Intantiate.driver.find_elements_by_xpath("//div[@class='location-column']")
-       print(len(district_name))
-       for district in district_name:
-           district.find_element_by_xpath("//div[@class='location-column']")
-           Dict_district["District"] = district.text
-           print(Dict_district)
-       Intantiate.driver.find_element_by_xpath("//div[@class='bookmarked' and contains(text(), 'MissionHumane.org')]").click()
-       sleep(2)
+       test_extract_district()
 
 
 def test_loadExcel():
@@ -46,10 +46,39 @@ def test_StateCompare():
         else:
             print(False)
 
+def test_extract_district():
+    district_name = Intantiate.driver.find_elements_by_xpath("//div[@class='location-column']")
+    print(len(district_name))
+    for district in district_name:
+        district.find_element_by_xpath("//div[@class='location-column']")
+        Dict_district["District"] = district.text
+        print(Dict_district)
+        district.click()
+        test_extract_resourse()
+    #Intantiate.driver.find_element_by_xpath("//div[@class='bookmarked' and contains(text(), 'MissionHumane.org')]").click()
+    sleep(2)
+
+def test_extract_resourse():
+    resourse_name = Intantiate.driver.find_elements_by_xpath("//div[@class='sc-hKFxyN kksiKu']")
+    print(len(resourse_name))
+    for resourse in resourse_name:
+        resourse.find_element_by_xpath("//div[@class='sc-hKFxyN kksiKu']")
+        Dict_resourse["Resourse"] = resourse.text
+        print(Dict_resourse)
+        resourse.click()
+        sleep(2)
+        test_extract_data()
 
 
-
-
+def test_extract_data():
+    data_count = Intantiate.driver.find_elements_by_xpath("//div[@class='MuiCardContent-root lead-container']")
+    print(len(data_count))
+    for data in data_count:
+        data.find_element_by_xpath("//div[@class='MuiCardContent-root lead-container']")
+        Dict_data["data"] = data.text
+        print(Dict_data)
+    Intantiate.driver.find_element_by_xpath("//div[@class='bookmarked' and contains(text(), 'MissionHumane.org')]").click()
+    sleep(2)
 
 # closing browser
 def test_close():
